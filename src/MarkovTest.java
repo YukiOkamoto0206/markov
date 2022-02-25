61e354787339d0eeacd049910d3cc9bf42e487f4
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-25 15:37:54 -0800

    add: test file

--- src/MarkovTest.java
+++ src/MarkovTest.java
@@ -1,3 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Title: MarkovTest.java
  * Abstract: Markov Text Generation
@@ -5,4 +11,34 @@
  * Date: Feb 24, 2022
  */
 public class MarkovTest {
+    private static final String PUNCTUATION = "__$";
+    private static final String PUNCTUATION_MARKS = ".!?";
+    private HashMap<String, ArrayList<String>> words;
+    private String prevWord;
+
+    @Test
+    void constructor() {
+        Markov markov = new Markov();
+    }
+
+    @Test
+    void getWords() {
+
+    }
+
+    @Test
+    void getSentence() {
+    }
+
+    @Test
+    void randomWord() {
+    }
+
+    @Test
+    void endsWithPunctuation() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
